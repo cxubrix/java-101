a7bb26748d10a96cb9e768a13c39c303e818a44d
Kaspars Plavins <kaspars.plavins@n-fuse.co>
2018-08-27 15:39:35 +0300

    Added CORS support to allow all in ToDo

--- todo/src/main/java/lv/rigacoding/todo/ToDoController.java
+++ todo/src/main/java/lv/rigacoding/todo/ToDoController.java
@@ -2,12 +2,14 @@ package lv.rigacoding.todo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequestMapping("/todo")
 @RestController
 public class ToDoController {
